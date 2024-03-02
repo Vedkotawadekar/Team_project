8d06f5f504622c48955786a3ab1e60cc5d5ed8b6
Vedkotawadekar <159684156+Vedkotawadekar@users.noreply.github.com>
2024-03-02 01:26:14 -0500

    log file demo

--- filter.py
+++ filter.py
@@ -4,7 +4,7 @@ def filter_even_numbers(numbers):
 def filter_odd_numbers(numbers):
     return [num for num in numbers if num % 2 != 0]
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = [1, 2, 3, 4, 5, 7, 8, 9, 10]
 
 even_numbers = filter_even_numbers(numbers)
 odd_numbers = filter_odd_numbers(numbers)
